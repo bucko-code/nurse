d4a52c966f22e06040f8321b04ba3283179ceea1
bucko-code <521344@pccm.ac.th>
2026-03-01 10:01:33 +0700

    Update finalscript.js

--- finalscript.js
+++ finalscript.js
@@ -323,7 +323,7 @@ function showResult() {
     if (T >= V && T >= J) {
         finalType = "T";
         resultImage.src = "images/T.jpg";
-        resultText.textContent= "จากคุณแพนด้า";
+        
     }
     else if (V >= T && V >= J) {
         finalType = "V";
